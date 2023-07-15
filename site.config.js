df086352b8b6014a2c028aec2962ea888722dff5
Serge william KADJO <spyhack225@users.noreply.github.com>
2023-07-15 14:44:07 -0400

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,35 +1,35 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "morethanmin",
+    name: "Next Notes",
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
-    role: "frontend developer",
-    bio: "I develop everything using node.",
-    email: "morethanmin.dev@gmail.com",
-    linkedin: "morethanmin",
-    github: "morethanmin",
-    instagram: "",
+    role: "Business Manager",
+    bio: "A wearable powered scribing app (www.next-notes.com) that helps ambulatory health practitioners create, manage and organize notes (SOAP, H&P) for online or in-person consultations.",
+    email: "hello@next-notes.com",
+    linkedin: "nextNotes",
+    github: "nextNotes",
+    instagram: "nextNotes",
   },
   projects: [
     {
-      name: `morethan-log`,
-      href: "https://github.com/morethanmin/morethan-log",
+      name: `Next Notes App`,
+      href: "https://next-notes.com",
     },
   ],
   // blog setting (required)
   blog: {
-    title: "morethan-log",
-    description: "welcome to morethan-log!",
+    title: "Blog | Next Notes",
+    description: "Welcome to Next Notes Blog!",
     theme: "auto", // ['light', 'dark', 'auto']
   },
 
   // CONFIG configration (required)
-  link: "https://morethan-log.vercel.app",
-  since: 2022, // If leave this empty, current year will be used.
+  link: "https://blog.next-notes.com",
+  since: 2023, // If leave this empty, current year will be used.
   lang: "en-US", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
   ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
   seo: {
-    keywords: ["Blog", "Website", "Notion"],
+    keywords: ["Blog", "NextNotes", "Healthcare", "Medical", "Doctors", "SOAP Notes", "H&P Notes", "Clinician"],
   },
 
   // notion configuration (required)
@@ -53,7 +53,7 @@ const CONFIG = {
   utterances: {
     enable: true,
     config: {
-      repo: "morethanmin/morethan-log",
+      repo: "Wearer-Lab/blog",
       "issue-term": "og:title",
       label: "💬 Utterances",
     },
